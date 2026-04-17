191667ec4db1c442c9400b37239bc054eb66ffb9
DevlM <dev.maudon@gmail.com>
2026-04-17 15:20:51 +0200

    Update base path to match GitHub repository name

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/countries-search/",
+  base: "/cloud_campus_countries_search/",
   plugins: [
     tanstackRouter({
       target: "react",
